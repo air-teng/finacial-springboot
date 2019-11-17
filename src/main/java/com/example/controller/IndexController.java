73eed9aa8c887c26c64b1a162416f18404f7033a
滕云飞 <paincloudfly@163.com>
2019-11-17 19:14:17 +0800

    后端代码首次推送

---
+++ src/main/java/com/example/controller/IndexController.java
@@ -0,0 +1,44 @@
+package com.example.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping("")
+@Controller
+public class IndexController {
+	
+	@RequestMapping(value= "")
+	@ResponseBody
+	public ModelAndView getIp(ModelAndView mav,HttpServletRequest request) {
+		String userIp = getIpAddress(request);
+		mav.addObject("userIp",userIp);
+		mav.setViewName("index");
+		return mav;
+	}
+	
+	
+	private String getIpAddress(HttpServletRequest request) { 
+	    String ip = request.getHeader("x-forwarded-for"); 
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+	      ip = request.getHeader("Proxy-Client-IP"); 
+	    } 
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+	      ip = request.getHeader("WL-Proxy-Client-IP"); 
+	    } 
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+	      ip = request.getHeader("HTTP_CLIENT_IP"); 
+	    } 
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+	      ip = request.getHeader("HTTP_X_FORWARDED_FOR"); 
+	    } 
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+	      ip = request.getRemoteAddr(); 
+	    } 
+	    return ip; 
+	}
+	
+}
